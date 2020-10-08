d3fc84d8e1437be62e8f3407fe3b642a2007a8f1
ronn <ronnie@nextdigital.es>
2020-10-08 15:53:22 +0200

    Add module for testing

---
+++ src/test/java/com/ronn/sbreferencearchitecture/SbReferenceArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronn.sbreferencearchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbReferenceArchitectureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
