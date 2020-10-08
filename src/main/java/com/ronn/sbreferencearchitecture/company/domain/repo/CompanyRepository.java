7417b708835286576aa6805bb692612ccc476666
ronn <ronnie@nextdigital.es>
2020-10-08 09:28:45 +0200

    Define domain repo

--- src/main/java/com/ronn/sbreferencearchitecture/company/domain/repo/CompanyRepository.java
+++ src/main/java/com/ronn/sbreferencearchitecture/company/domain/repo/CompanyRepository.java
@@ -4,15 +4,20 @@ import com.ronn.sbreferencearchitecture.company.domain.model.Company;
 import com.ronn.sbreferencearchitecture.company.domain.model.Employee;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CompanyRepository {
 
-  Company find(UUID id);
-  List<Company> getAll();
+  Optional<Company> find(UUID id);
+
+  List<Company> getAll(int page, int size);
+
   UUID save(Company company);
 
-  Employee findEmployee(UUID id);
-  List<Employee> getAllEmployees();
+  Optional<Employee> findEmployee(UUID id);
+
+  List<Employee> getAllEmployees(int page, int size);
+
   UUID save(Employee employee);
 }
